44ff22765b8f616cb1b4e26d8469971be0e2e5a5
AngryWaves <48641379+AngryWaves@users.noreply.github.com>
2023-05-09 21:14:08 +0800

    Add files via upload

---
+++ loss.py
@@ -0,0 +1,106 @@
+import torch.nn as nn
+import numpy as np
+import  torch
+import torch.nn.functional as F
+
+
+class DiceLoss(nn.Module):
+    def __init__(self, n_classes):
+        super(DiceLoss, self).__init__()
+        self.n_classes = n_classes
+
+    def _one_hot_encoder(self, input_tensor):
+        tensor_list = []
+        for i in range(self.n_classes):
+            temp_prob = input_tensor == i  # * torch.ones_like(input_tensor)
+            tensor_list.append(temp_prob.unsqueeze(1))
+        output_tensor = torch.cat(tensor_list, dim=1)
+        return output_tensor.float()
+
+    def _dice_loss(self, score, target):
+        target = target.float()
+        smooth = 1e-5
+        intersect = torch.sum(score * target)
+        y_sum = torch.sum(target * target)
+        z_sum = torch.sum(score * score)
+        loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+        loss = 1 - loss
+        return loss
+
+    def forward(self, inputs, target, weight=None, softmax=False):
+        if softmax:
+            inputs = torch.softmax(inputs, dim=1)
+        else:
+            inputs = torch.sigmoid(inputs)
+        # target = self._one_hot_encoder(target)
+        if weight is None:
+            weight = [1] * self.n_classes
+        assert inputs.size() == target.size(), 'predict {} & target {} shape do not match'.format(inputs.size(), target.size())
+        class_wise_dice = []
+        loss = 0.0
+        for i in range(0, self.n_classes):
+            dice = self._dice_loss(inputs[:, i], target[:, i])
+            class_wise_dice.append(1.0 - dice.item())
+            loss += dice * weight[i]
+        return loss / self.n_classes
+
+
+def SoftIoULoss( pred, target):
+        # Old One
+        pred = torch.sigmoid(pred)
+        smooth = 1
+
+        # print("pred.shape: ", pred.shape)
+        # print("target.shape: ", target.shape)
+
+        intersection = pred * target
+        loss = (intersection.sum() + smooth) / (pred.sum() + target.sum() -intersection.sum() + smooth)
+
+        # loss = (intersection.sum(axis=(1, 2, 3)) + smooth) / \
+        #        (pred.sum(axis=(1, 2, 3)) + target.sum(axis=(1, 2, 3))
+        #         - intersection.sum(axis=(1, 2, 3)) + smooth)
+
+        loss = 1 - loss.mean()
+        # loss = (1 - loss).mean()
+
+        return loss
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def Dice_loss(inputs, target, beta=1, smooth=1e-5):
+    n, c, h, w = inputs.size()
+    nt, ht, wt, ct = target.size()
+    if h != ht and w != wt:
+        inputs = F.interpolate(inputs, size=(ht, wt), mode="bilinear", align_corners=True)
+
+    temp_inputs = torch.softmax(inputs.transpose(1, 2).transpose(2, 3).contiguous().view(n, -1, c), -1)
+    temp_target = target.view(n, -1, ct)
+
+    # --------------------------------------------#
+    #   计算dice loss
+    # --------------------------------------------#
+    tp = torch.sum(temp_target[..., :-1] * temp_inputs, axis=[0, 1])
+    fp = torch.sum(temp_inputs, axis=[0, 1]) - tp
+    fn = torch.sum(temp_target[..., :-1], axis=[0, 1]) - tp
+
+    score = ((1 + beta ** 2) * tp + smooth) / ((1 + beta ** 2) * tp + beta ** 2 * fn + fp + smooth)
+    dice_loss = 1 - torch.mean(score)
+    return dice_loss
+
